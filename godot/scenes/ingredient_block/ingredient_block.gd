538bbb57e4f4cdbc48d5f6d060471b508bbe1ec4
Tyler Chase <tylercchase@gmail.com>
2024-07-19 17:50:12 -0400

    style: darken base dragged block

--- godot/scenes/ingredient_block/ingredient_block.gd
+++ godot/scenes/ingredient_block/ingredient_block.gd
@@ -16,4 +16,10 @@ func _get_drag_data(_at_position:Vector2)->Variant:
 	var drag_data = ItemDrag.new(self, block_data, preview)
 	set_drag_preview(drag_data.preview)
 
+	modulate = Color(1.0,1.0,1.0,0.4)
+
 	return drag_data
+
+func _notification(what:int)->void:
+	if what == Node.NOTIFICATION_DRAG_END:
+		modulate = Color.WHITE
