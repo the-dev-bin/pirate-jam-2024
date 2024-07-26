2ab28fd815052cbfc89b70b79d34a7e23ccac83b
Tyler Chase <tylercchase@gmail.com>
2024-07-26 18:59:05 -0400

    fix: recursive dependency

--- godot/ui/menues/combat_end/combat_end.gd
+++ godot/ui/menues/combat_end/combat_end.gd
@@ -2,7 +2,6 @@ class_name CombatEndScene
 extends Control
 
 @export var ingredient_button_scene: PackedScene
-@export var map_scene: PackedScene
 
 @onready var interaction_container: VBoxContainer = %InteractionContainer
 @onready var map_button: Button = %MapButton
@@ -18,4 +17,4 @@ func setup(loot: LootTable) -> void:
 	interaction_container.add_child(temp)
 
 func _on_map_button_pressed() -> void:
-	get_tree().change_scene_to_packed(map_scene) # evemtually have some check here to see if there's some leftover stuff
+	get_tree().change_scene_to_file("res://scenes/map/map.tscn") # evemtually have some check here to see if there's some leftover stuff
