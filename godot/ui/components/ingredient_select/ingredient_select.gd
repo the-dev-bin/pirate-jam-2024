4d11beefeeecd81a3cef2db81427066e5eee64d5
Tyler Chase <tylercchase@gmail.com>
2024-07-26 21:14:00 -0400

    feat: add skip button

--- godot/ui/components/ingredient_select/ingredient_select.gd
+++ godot/ui/components/ingredient_select/ingredient_select.gd
@@ -4,12 +4,14 @@ extends CanvasLayer
 @export var ingredient_card_scene: PackedScene
 
 @onready var selection_container: HBoxContainer = %SelectionContainer
+@onready var skip_button: Button = %SkipButton
 
 signal card_selected
 
 func _ready() -> void:
 	for child in %SelectionContainer.get_children():
 		child.queue_free()
+	skip_button.pressed.connect(_on_skip_button)
 
 func setup(ingredients: Array[Ingredient]):
 	for ingredient in ingredients:
@@ -21,5 +23,8 @@ func setup(ingredients: Array[Ingredient]):
 
 func _on_card_selected(ingredient: Ingredient):
 	State.player_deck.push_back(ingredient)
+	card_selected.emit()
+
+func _on_skip_button() -> void:
 	card_selected.emit()
 
